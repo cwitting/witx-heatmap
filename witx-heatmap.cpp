65ddf3a6f5b9163d4f5625ee3d582cd755e9b520
Christian Witting <witting.chr@gmail.com>
2026-08-29 20:38:16 +0200

    Initial commit

---
+++ witx-heatmap.cpp
@@ -0,0 +1,22 @@
+// #include <omp.h>
+
+#include <cmath>
+#include <cstdio>
+#include <ctime>
+#include <osrm/coordinate.hpp>
+#include <osrm/engine_config.hpp>
+#include <osrm/json_container.hpp>
+#include <osrm/nearest_parameters.hpp>
+#include <osrm/osrm.hpp>
+#include <osrm/route_parameters.hpp>
+#include <osrm/status.hpp>
+
+#include "route_planner_lib.cpp"
+
+// ============================================================================
+// Main program
+// ============================================================================
+int main(int argc, char* argv[]) {
+  // Print program name
+  fprintf(stderr, "Witx Heatmap Route Planner\n");
+}
