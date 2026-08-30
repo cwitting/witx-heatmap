fff5f6f75c8fa781371efa721cdd773bfef7b40f
Christian Witting <witting.chr@gmail.com>
2026-08-30 16:49:07 +0200

    test

--- witx-heatmap.cpp
+++ witx-heatmap.cpp
@@ -17,6 +17,7 @@
 
 #include "engine/api/match_parameters.hpp"
 #include "json.hpp"
+#include "util/json_renderer.hpp"
 
 struct Coordinate {
   double lat{};
@@ -60,8 +61,115 @@ std::vector<Route> load_all_routes() {
 
   return routes;
 }
+// Using chrono timer
+class TimerLog {
+ public:
+  TimerLog(const std::string& name) : name_(name), start_(std::chrono::high_resolution_clock::now()) {}
+
+  ~TimerLog() { stop(); }
+
+  void stop() {
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start_;
+    fprintf(stderr, "%s took %.6f seconds\n", name_.c_str(), elapsed.count());
+  }
+
+ private:
+  std::string name_;
+  std::chrono::time_point<std::chrono::high_resolution_clock> start_;
+};
 
 class RouteMatcher {
+ public:
+  RouteMatcher() {
+    // Configure OSRM
+    osrm::EngineConfig config;
+
+    config.storage_config = {OSRM_DATA_DIR};
+    config.use_shared_memory = false;
+    config.algorithm = osrm::EngineConfig::Algorithm::MLD;
+
+    // Create OSRM instance
+    fprintf(stderr, "Initializing OSRM with data from: %s\n", OSRM_DATA_DIR);
+    TimerLog timer("OSRM initialization");
+    osrm_ = std::make_unique<osrm::OSRM>(config);
+  }
+
+  // Match routes to the road network using OSRM
+  std::optional<MatchedRoute> matchRoute(const Route& route) {
+    osrm::RouteParameters params;
+    for (const auto& coord : route) {
+      params.coordinates.emplace_back(osrm::util::FloatLongitude(coord.lon), osrm::util::FloatLatitude(coord.lat));
+    }
+
+    osrm::json::Object result;
+    osrm::MatchParameters match_params;
+    match_params.coordinates = params.coordinates;
+    match_params.annotations = true;
+    match_params.annotations_type = osrm::MatchParameters::AnnotationsType::Nodes;
+    const auto status = osrm_->Match(match_params, result);
+
+    if (status != osrm::Status::Ok) {
+      fprintf(stderr, "OSRM Match failed with status: %d\n", static_cast<int>(status));
+      return std::nullopt;
+    }
+
+    MatchedRoute matched_route;
+    matched_route.route = route;
+
+    // print the raw json result to stderr
+    std::string json_str;
+    osrm::util::json::render(json_str, result);
+    fprintf(stderr, "OSRM Match result: %s\n", json_str.c_str());
+
+    // Add the osm way ids to the matched_route.osm_route vector
+
+    if (result.values.count("matchings") > 0) {
+      const auto& matchings = std::get<osrm::util::json::Array>(result.values["matchings"]);
+      for (const auto& matching : matchings.values) {
+        const auto& matching_obj = std::get<osrm::util::json::Object>(matching);
+        if (matching_obj.values.count("legs") > 0) {
+          const auto& legs = std::get<osrm::util::json::Array>(matching_obj.values.at("legs"));
+          for (const auto& leg : legs.values) {
+            const auto& leg_obj = std::get<osrm::util::json::Object>(leg);
+            if (leg_obj.values.count("annotation") > 0) {
+              const auto& annotation = std::get<osrm::util::json::Object>(leg_obj.values.at("annotation"));
+              if (annotation.values.count("nodes") > 0) {
+                const auto& nodes = std::get<osrm::util::json::Array>(annotation.values.at("nodes"));
+                for (const auto& node : nodes.values) {
+                  matched_route.osm_route.push_back(std::to_string(std::get<osrm::util::json::Number>(node).value));
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return matched_route;
+  }
+
+  std::vector<MatchedRoute> matchAllRoutes(const std::vector<Route>& routes) {
+    std::vector<MatchedRoute> matched_routes;
+    int matched_count = 0;
+    int total_count = 0;
+    for (const auto& route : routes) {
+      TimerLog timer("Matching route " + std::to_string(matched_count + 1) + "/" + std::to_string(total_count + 1) +
+                     "/" + std::to_string(routes.size()));
+      auto matched_route = matchRoute(route);
+      total_count++;
+      if (!matched_route) {
+        continue;
+      }
+      matched_routes.push_back(*matched_route);
+      matched_count++;
+      if (matched_count >= 30) {
+        break;
+      }
+    }
+    return matched_routes;
+  }
+
  private:
   std::unique_ptr<osrm::OSRM> osrm_;
 
@@ -104,75 +212,6 @@ class RouteMatcher {
 
     return coordinates;
   }
-
- public:
-  RouteMatcher() {
-    // Configure OSRM
-    osrm::EngineConfig config;
-
-    config.storage_config = {OSRM_DATA_DIR};
-    config.use_shared_memory = false;
-    config.algorithm = osrm::EngineConfig::Algorithm::MLD;
-
-    // Create OSRM instance
-    osrm_ = std::make_unique<osrm::OSRM>(config);
-  }
-
-  // Match routes to the road network using OSRM
-  std::optional<MatchedRoute> matchRoute(const Route& route) {
-    osrm::RouteParameters params;
-    for (const auto& coord : route) {
-      params.coordinates.emplace_back(osrm::util::FloatLongitude(coord.lon), osrm::util::FloatLatitude(coord.lat));
-    }
-
-    osrm::json::Object result;
-    osrm::MatchParameters match_params;
-    match_params.coordinates = params.coordinates;
-    const auto status = osrm_->Match(match_params, result);
-
-    if (status != osrm::Status::Ok) {
-      fprintf(stderr, "OSRM Match failed with status: %d\n", static_cast<int>(status));
-      return std::nullopt;
-    }
-
-    MatchedRoute matched_route;
-    matched_route.route = route;
-    // Add the osm way ids to the matched_route.osm_route vector
-    if (result.values.count("matchings") > 0) {
-      const auto& matchings = std::get<osrm::util::json::Array>(result.values["matchings"]);
-      for (const auto& matching : matchings.values) {
-        const auto& matching_obj = std::get<osrm::util::json::Object>(matching);
-        if (matching_obj.values.count("legs") > 0) {
-          const auto& legs = std::get<osrm::util::json::Array>(matching_obj.values.at("legs"));
-          for (const auto& leg : legs.values) {
-            const auto& leg_obj = std::get<osrm::util::json::Object>(leg);
-            if (leg_obj.values.count("annotation") > 0) {
-              const auto& annotation = std::get<osrm::util::json::Object>(leg_obj.values.at("annotation"));
-              if (annotation.values.count("nodes") > 0) {
-                const auto& nodes = std::get<osrm::util::json::Array>(annotation.values.at("nodes"));
-                for (const auto& node : nodes.values) {
-                  matched_route.osm_route.push_back(std::to_string(std::get<osrm::util::json::Number>(node).value));
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
-    return matched_route;
-  }
-
-  std::vector<MatchedRoute> matchAllRoutes(const std::vector<Route>& routes) {
-    std::vector<MatchedRoute> matched_routes;
-    for (const auto& route : routes) {
-      auto matched_route = matchRoute(route);
-      if (matched_route) {
-        matched_routes.push_back(*matched_route);
-      }
-    }
-    return matched_routes;
-  }
 };
 
 // ============================================================================
@@ -185,9 +224,11 @@ int main(int argc, char* argv[]) {
   RouteMatcher matcher;
   auto matched_routes = matcher.matchAllRoutes(routes);
   fprintf(stderr, "Matched %zu routes out of %zu\n", matched_routes.size(), routes.size());
-  auto matched_route = matched_routes.front();
-  for (const auto& way_id : matched_route.osm_route) {
-    std::cout << way_id << " ";
-  }
+  // for (const auto& matched_route : matched_routes) {
+  //   fprintf(stderr, "Matched route with %zu way ids\n", matched_route.osm_route.size());
+  //   for (const auto& way_id : matched_route.osm_route) {
+  //     std::cout << way_id << " ";
+  //   }
+  // }
   std::cout << std::endl;
 }
